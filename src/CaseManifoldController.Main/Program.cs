4888f2100b92bc7d4a65c9d4ad0860467e7c3b19
Jens Berfenfeldt <jens@berfenfeldt.com>
2020-11-29 15:04:52 +0100

    can move servo

--- src/CaseManifoldController.Main/Program.cs
+++ src/CaseManifoldController.Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 
 namespace CaseManifoldController
 {
@@ -6,10 +7,47 @@ namespace CaseManifoldController
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("!");
+            while (true)
+            {
+                var position = int.Parse(Console.ReadLine());
+                SetServoPosition("COM4", 9600, 5, position, 20);
+            }
+        }
 
+        public static void SetServoPosition(string ComPort, int BaudRate, int ServoNumber, int ServoPos, int servoSpeed)
+        {
+            SerialPort port = new SerialPort(ComPort, BaudRate, Parity.None, 8, StopBits.One);
+            port.Open();
 
-            Console.ReadKey();
+            string ServoNumberHex = ServoNumber.ToString("X");
+            byte ServoNumberByte = byte.Parse(ServoNumberHex, System.Globalization.NumberStyles.HexNumber);
+
+            SetSpeed(port, ServoNumberByte, servoSpeed);
+            SetPosition(port, ServoNumberByte, ServoPos);
+
+            port.Close();
+        }
+
+        private static void SetSpeed(SerialPort port, byte ServoNumberByte, int servoSpeed)
+        {
+            string ServoSpeedHex = servoSpeed.ToString("X");
+            byte SpeedByte = byte.Parse(ServoSpeedHex, System.Globalization.NumberStyles.HexNumber);
+
+            var data = new byte[] {
+                0x87,
+                ServoNumberByte,    
+                SpeedByte,        
+                0x00                
+            };
+            port.Write(data, 0, 4);
+        }
+
+        private static void SetPosition(SerialPort port, byte ServoNumberByte, int ServoPos)
+        {
+            string ServoPosHex = ServoPos.ToString("X");
+            byte PositionByte = byte.Parse(ServoPosHex, System.Globalization.NumberStyles.HexNumber);
+
+            port.Write(new byte[] { 0xFF, ServoNumberByte, PositionByte }, 0, 3);
         }
     }
 }
